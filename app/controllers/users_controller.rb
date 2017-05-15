89dda43a68bb19caf27a5668e5305b4ab5c0214f
yarahana <ktn.xg3@gmail.com>
2017-05-15 17:57:28 +0900

    面談の終わりに

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-      user = User.find(params[:id])
-      @name = current_user.name
-      @reviews = user.reviews.order("created_at DESC")
+      @user = User.find(params[:id])
+      @reviews = @user.reviews.order("created_at DESC")
   end
 end
